37ecfc34a1bdd8d7a19de5b4ada959cb03e84bd0
Corbin Ball <c.ball@me.com>
2021-08-21 15:37:13 -0500

    fixed that if character prompt is true, adds into empty array, if false does nothing

--- script.js
+++ script.js
@@ -14,10 +14,10 @@ function writePassword() {
 
 function generatePassword() {
 
-  var alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  var number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  var special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+  var alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+  var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
+  var number = [1, 2, 3, 4, 5, 6, 7, 8, 9,];
+  var special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~",];
   var confrimedTrues = [];
   var pwLength;
   var enter; 
@@ -39,45 +39,50 @@ function generatePassword() {
 
 };
 
-        getLower =  confirm("How bout lower case letters in your password?");
-        console.log(getLower);
-            if (getLower === true); {
-                confrimedTrues = confrimedTrues.concat(alpha);
-                console.log(confrimedTrues);
-};             
+    getLower =  confirm("How bout lower case letters in your password?");
+    console.log(getLower);
+    if (getLower) {
+        confrimedTrues = confrimedTrues.concat(alpha);
+        console.log(confrimedTrues);
+    }
+                 
 
 
-        getUpper = confirm("Ok cool. Do you want upper case letters in your password?");
-        console.log(getUpper);
-            if (getUpper === true); {
-                confrimedTrues += confrimedTrues.concat(alphaUpper);
-                console.log(confrimedTrues);
+    getUpper = confirm("Ok cool. Do you want upper case letters in your password?");
+    console.log(getUpper);
+    if (getUpper) {
+        confrimedTrues = confrimedTrues.concat(alphaUpper);
+        console.log(confrimedTrues);
 };
 
         
-        getNumber = confirm("Ok cool, cool. Do you want numbers in your password?");
-        console.log(getNumber);
-            if (getNumber === true); {
-                confrimedTrues += (confrimedTrues.concat(number));
-                console.log(confrimedTrues);
+    getNumber = confirm("Ok cool, cool. Do you want numbers in your password?");
+    console.log(getNumber);
+    if (getNumber) {
+        confrimedTrues = (confrimedTrues.concat(number));
+        console.log(confrimedTrues);
 };
 
-        getSpecial = confirm("Alright cool. Finally, do you want special characters in your password?");
-        console.log(getSpecial);
-            if (getSpecial === true); {
-             confrimedTrues += (confrimedTrues.concat(special));
-                console.log(confrimedTrues);
+    getSpecial = confirm("Alright cool. Finally, do you want special characters in your password?");
+    console.log(getSpecial);
+    if (getSpecial) {
+        confrimedTrues = (confrimedTrues.concat(special));
+        console.log(confrimedTrues);
+    }
+console.log(confrimedTrues);
+
+
+    if (getLower && getUpper & getNumber && getNumber) {
+        generatePassword = Math.floor(Math.random() + confrimedTrues.pwLength);
+        console.log(generatePassword);
 };
 
     if (!getLower && !getUpper && !getNumber && !getSpecial) {
       choices = alert("I can't work with that! Sorry but, you'll have to try again.");
 }   
-    if (getLower && getUpper & getNumber && getNumber) {
-        makePw = Math.floor(Math.random() + confrimedTrues.pwLength);
-        generatePassword = confrimedTrues[makePw];
-        console.log(generatePassword);
+
     };
-};
+
 
 
 // Add event listener to generate button
